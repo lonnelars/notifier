2440ba65931ca2955f0e7d2030bbc3d2624124f8
Lars Lønne <lars.lonne@kantega.no>
2018-01-22 20:23:34 +0100

    feat: clear interval

--- index.js
+++ index.js
@@ -1,16 +1,22 @@
+#!/usr/bin/env node
+
 const notifier = require("node-notifier");
 
 function notify() {
   notifier.notify({
     title: "Husk å føre timer",
     message: "Hva har du gjort den siste timen?",
-    wait: true
+    timeout: 120
   });
 }
 
 function notifyAtTime(date) {
-  const now = new Date();
-  setTimeout(notify, date - now);
+  const interval = setInterval(() => {
+    if (Math.abs(date - new Date()) < 10000) {
+      notify();
+      clearInterval(interval);
+    }
+  }, 10000);
 }
 
 function quarterToFour() {
@@ -20,10 +26,19 @@ function quarterToFour() {
   return d;
 }
 
+function endOfDay() {
+  const d = new Date();
+  d.setSeconds(0);
+  d.setMinutes(0);
+  d.setHours(16);
+  return d;
+}
+
 function main() {
   const times = [9, 10, 11, 12, 13, 14, 15]
     .map(n => {
       const d = new Date();
+      d.setSeconds(0);
       d.setMinutes(0);
       d.setHours(n);
       return d;
@@ -31,6 +46,11 @@ function main() {
     .concat(quarterToFour())
     .filter(time => time - new Date() > 0);
   times.forEach(time => notifyAtTime(time));
+  setInterval(() => {
+    if (new Date() - endOfDay() > 0) {
+      process.exit(0);
+    }
+  }, 60 * 1000);
 }
 
 main();
